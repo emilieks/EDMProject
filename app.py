c66f0bbc37001ecda852b197a2cfbc9069deb03c
Emilie K Sivertsen <emilieksivertsen@Emilies-MacBook-Air.local>
2025-05-29 16:40:56 +0200

    Try out some functionality in streamlit app

--- app.py
+++ app.py
@@ -1,23 +1,14 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
 
-st.write("# Welcome to test")
+st.write("# Valenbisi Statistics and Predictions")
 
-st.write("Hello, Streamlit!")
+col1, col2 = st.columns(2)
 
-var = st.text_input("Enter some text:")
+with col1:
+    st.page_link("pages/1_Predict_Free_Parking.py", 
+                 label="Predict Free Parking")
 
-st.write("You entered:", var)
-
-# Add a slider
-x = st.slider("Pick a number", 0, 100, 25)
-st.write("You selected:", x)
-
-# Show a chart
-df = pd.DataFrame({
-    'x': np.arange(10),
-    'y': np.random.randn(10)
-})
-st.line_chart(df)
+with col2:
+    st.page_link("pages/2_test2.py", 
+                 label="Test 2")
 
--- pages/1_test.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-import joblib
-
-st.title("TESTESTETSTETTSTTETSTE")
-
-model, feature_names = joblib.load("model.pkl")
-
